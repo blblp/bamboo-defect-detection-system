6d29eb36f8a928ebd18db79e946f3585447450b2
blblp <103884154+blblp@users.noreply.github.com>
2026-05-13 13:16:39 +0800

    Update UI.sh

--- UI.sh
+++ UI.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# ================= 配置区域 (请修改为你的实际路径) =================
+# ================= 配置区域 (修改为实际路径) =================
 CONDA_INIT="/home/jetson1101/miniforge3/etc/profile.d/conda.sh"
-# 新增：定义conda环境名称/路径
+# 定义conda环境名称/路径
 CONDA_ENV="/home/jetson1101/miniforge3/envs/yolov8_py38"
-# 2. recv_data.py 所在的文件夹路径
+# recv_data.py 所在的文件夹路径
 RECV_DIR="/home/jetson1101/bamboo/communication"
 
-# 3. main_pca_svm.py 所在的文件夹路径 (如果是同一个文件夹就写一样的)
 SVM_DIR="/home/jetson1101/bamboo/UI"
 
 SUDO_CMD_1='sudo -v'
@@ -19,12 +18,12 @@ SUDO_PWD='123'
 
 echo ">>> 开始初始化系统..."
 
-# 👇 新增绝杀功能 1：启动前，先无情清理所有残留的“幽灵进程”
-echo ">>> 0. 正在无情清理历史残留的 TRT 幽灵进程..."
+#启动前，先清理所有残留的幽灵进程
+echo ">>> 0. 正在清理历史残留的 TRT 幽灵进程..."
 pkill -f trt_server.py
 sleep 1
 
-# 1. 获取 sudo 权限 (脚本运行时只需输入一次密码)
+# 1. 获取 sudo 权限 
 sudo -v
 # expect -c "
 # spawn $SUDO_CMD_1
@@ -73,17 +72,17 @@ xhost +local:root
 
 # ========================== 启动程序 ==========================
 echo ">>> 1. 正在唤醒底层 C++ TensorRT 推理服务 (原生 Python3)..."
-# 重新引入一下环境变量，防止 sudo 弄丢了路径
+# 重新引入一下环境变量，防止 sudo 弄丢路径
 export PATH=/usr/local/cuda/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 
-# 👇 新增绝杀功能 2：弹出一个新的独立终端来运行 trt_server，方便你实时监控方差和检测日志
+# 弹出一个新的独立终端来运行 trt_server，方便实时监控方差和检测日志
 gnome-terminal -- bash -c "python3 /home/jetson1101/bamboo/UI/trt_server.py; exec bash"
 
-echo ">>> 等待 TensorRT 引擎预热 (5秒)..."
+echo ">>> 等待 TensorRT 引擎预热 ..."
 sleep 5
 
-echo ">>> 2. 正在启动前端监控 UI 界面 (yolov8_py38)..."
+echo ">>> 2. 正在启动前端监控 UI 界面 ..."
 source ~/.bashrc
 source /home/jetson1101/miniforge3/etc/profile.d/conda.sh
 cd "$(dirname "$0")"
@@ -92,6 +91,6 @@ python /home/jetson1101/bamboo/UI/exe-2.py
 
 # ========================== 退出清理 ==========================
 echo ">>> UI 界面已关闭，正在同步清理底层驻留的 TRT 服务..."
-# 退出时再次全局扫荡，彻底打扫战场，替代原本的 kill -9 $TRT_PID
+
 pkill -f trt_server.py
 echo ">>> 所有程序已安全退出，系统已释放显存！"
